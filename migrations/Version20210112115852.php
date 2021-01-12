7b1c016369de3b6585de04a1836bb1cebc3cf357
CHAUVIN Tom <chauvintom.pro@gmail.com>
2021-01-12 13:02:40 +0100

    problem migration

---
+++ migrations/Version20210112115852.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210112115852 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE adherent ADD famille_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE adherent ADD CONSTRAINT FK_90D3F06097A77B84 FOREIGN KEY (famille_id) REFERENCES famille (id)');
+        $this->addSql('CREATE INDEX IDX_90D3F06097A77B84 ON adherent (famille_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE adherent DROP FOREIGN KEY FK_90D3F06097A77B84');
+        $this->addSql('DROP INDEX IDX_90D3F06097A77B84 ON adherent');
+        $this->addSql('ALTER TABLE adherent DROP famille_id');
+    }
+}
