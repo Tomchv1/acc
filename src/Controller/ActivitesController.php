a6edda16b4f53d7ca8c9c4ed7d0d1120c2ef1328
CHAUVIN Tom <chauvintom.pro@gmail.com>
2021-01-14 13:31:11 +0100

    Lister et consulter activites

--- src/Controller/ActivitesController.php
+++ src/Controller/ActivitesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Activites;
 
 class ActivitesController extends AbstractController
 {
@@ -16,5 +17,32 @@ class ActivitesController extends AbstractController
         return $this->render('activites/index.html.twig', [
             'controller_name' => 'ActivitesController',
         ]);
+    }
+
+    public function listerActivites()
+    {
+        $activites = $this->getDoctrine()
+        ->getRepository(Activites::class)
+        ->findAll();
+         return $this->render('activites/listerActivites.html.twig', [
+            'pActivites' => $activites,]);  
+    }
+
+    public function consulterActivites($activites_id)
+    {
+    	$activites = $this->getDoctrine()
+        ->getRepository(Activites::class)
+        ->find($activites_id);
+
+        var_dump($activites);
+        // $activites = $this->getDoctrine()
+        // ->getRepository(Activites::class)
+        // ->find($adherent_id);
+
+        // $responsables = $this->getDoctrine()
+        // ->getRepository(Responsable::class)
+        // ->findByAdherent($adherent);
+
+        return $this->render('activites/consulterActivites.html.twig', ['consulter' => $activites,]);
     }
 }
